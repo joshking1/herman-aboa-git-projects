f44e1594331edf35895765e90e44c5e3ba84197e
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-11-23 09:31:54 -0500

    Create patch-management.sh

---
+++ patch-management.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Exiting."
+   exit 1
+fi
+
+# Update package information
+apt update
+
+# Upgrade installed packages
+apt upgrade -y
+
+# Clean up unused packages and cached files
+apt autoremove -y
+apt clean
+
+echo "Patch management completed."
